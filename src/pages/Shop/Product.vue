e7a6f6048a4efe1446c4fde0db7a6a19331f2b6d
caicheng <caicheng@hooook.com>
2020-02-20 16:37:25 +0800

    ..

--- src/pages/Shop/Product.vue
+++ src/pages/Shop/Product.vue
@@ -1,20 +1,38 @@
 <template>
-  <div>
-    产品管理
+  <div class="q-pa-md">
+    <q-table
+      :data="data"
+      :columns="columns"
+      title="产品管理"
+      :rows-per-page-options="[]"
+      row-key="code"
+      :loading="tableLoading"
+    >
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td key="code" :props="props">
+            {{ props.row.code }}
+          </q-td>
+          <q-td key="name" :props="props">
+            {{ props.row.name }}
+          </q-td>
+          <q-td key="single_price" :props="props">
+            {{ props.row.single_price }}元/1份
+          </q-td>
+          <q-td>
+            <q-btn color="red" round @click="deleteAction(props.row.id)" label="删除"/>
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
   </div>
 </template>
 
 <script>
   const columns = [
-    { name: 'id', align: 'center', label: '订单号码', field: 'id' },
-    { name: 'order_products', align: 'center', label: '订单产品', field: 'order_products' },
-    { name: 'order_name', align: 'center', label: '联系人', field: 'order_name' },
-    { name: 'order_phone', align: 'center', label: '联系电话', field: 'order_phone' },
-    { name: 'order_address', align: 'center', label: '地址', field: 'order_address' },
-    { name: 'order_address_detail', align: 'center', label: '详细地址', field: 'order_address_detail' },
-    { name: 'order_remark', align: 'center', label: '用户备注', field: 'order_remark' },
-    { name: 'created_at', align: 'center', label: '提交时间', field: 'created_at' },
-    { name: 'order_status', align: 'center', label: '状态标记', field: 'order_status' },
+    { name: 'code', align: 'center', label: 'id', field: 'code' },
+    { name: 'name', align: 'center', label: '名称', field: 'name' },
+    { name: 'single_price', align: 'center', label: '单价', field: 'single_price' },
     { name: 'handle', align: 'center', label: '操作', field: 'handle' },
   ]
   const data = []
@@ -24,11 +42,27 @@
         data,
         columns,
         tableLoading: true,
-        searchProductName: '',
       }
     },
-    methods: {},
+    methods: {
+      loadData () {
+        this.$axios.get('/shop/product', {})
+          .then((res) => {
+            // let res = response.data
+            if (res.status_code === 200) {
+              this.data = res.data
+            }
+          })
+          .catch((e) => {
+          })
+        this.tableLoading = false
+      },
+      deleteAction (id) {
+
+      }
+    },
     created () {
+      this.loadData()
     }
   }
 </script>
