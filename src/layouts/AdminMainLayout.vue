d37c4083dd57de962cb97c05d70c341c2d6b0fbf
caicheng <caicheng@hooook.com>
2020-02-21 20:41:09 +0800

    侧边栏修复

--- src/layouts/AdminMainLayout.vue
+++ src/layouts/AdminMainLayout.vue
@@ -1,23 +1,40 @@
 <template>
   <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-black">
+      <q-toolbar>
+        <q-btn flat @click="left = !left" round dense icon="menu" />
+        <q-toolbar-title>Header</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
 
-    <q-drawer show-if-above v-model="left" side="left" bordered>
-      <q-list bordered separator>
-        <q-item clickable v-ripple to="/admin/" exact>
-          <q-item-section>订单管理</q-item-section>
-        </q-item>
-        <q-item clickable v-ripple to="/admin/product" exact>
-          <q-item-section>
-            产品管理
-          </q-item-section>
-        </q-item>
-        <q-item clickable v-ripple to="/admin/address" exact>
-          <q-item-section>
-            地址管理
-          </q-item-section>
-        </q-item>
-      </q-list>
+    <q-drawer
+      v-model="left"
+      show-if-above
+      :width="200"
+      :breakpoint="500"
+      bordered
+      content-class="bg-grey-3"
+    >
+      <q-scroll-area class="fit">
+        <q-list bordered separator>
+          <q-item clickable v-ripple to="/admin/" exact>
+            <q-item-section>订单管理</q-item-section>
+          </q-item>
+          <q-item clickable v-ripple to="/admin/product" exact>
+            <q-item-section>
+              产品管理
+            </q-item-section>
+          </q-item>
+          <q-item clickable v-ripple to="/admin/address" exact>
+            <q-item-section>
+              地址管理
+            </q-item-section>
+          </q-item>
+        </q-list>
+
+      </q-scroll-area>
     </q-drawer>
+
 
     <q-page-container>
       <router-view/>
